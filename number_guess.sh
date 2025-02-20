16f99d541f7e0086597e0309de47e6ac2d4fb240
Musa Tshabalala <3rdksubi@gmail.com>
2025-02-20 02:49:05 +0000

    feat: collect user data

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+export PGPASSWORD="M722319355m"
+
+PSQL="psql --username=musa_tshabalala dbname=number_guess --no-align -t -c"
+
+function LETS_PLAY {
+    echo Enter your username:
+    read NAME
+
+    USER_EXISTS=$($PSQL "SELECT user_name FROM users WHERE user_name = '$NAME'")
+
+    if [[ -z $USER_EXISTS ]]
+    then
+        INSERT_NEW_USER=$($PSQL "INSERT INTO users (user_name) VALUES ('$NAME')")
+        USER_EXISTS=$($PSQL "SELECT user_name FROM users WHERE user_name = '$NAME'")
+        echo Your account has been successfully created! $NAME.
+    else
+        echo Welcome, $NAME
+    fi
+}
+
+LETS_PLAY
